d82bc63a154237517676bac3adc4ad683f1cf341
Kamshory <kamshory@yahoo.com>
2020-10-07 05:17:17 +0700

    Add Index

--- message_broker.sql
+++ message_broker.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `data` (
   `channel` varchar(100) DEFAULT NULL,
   `data` longtext,
   `created` datetime DEFAULT NULL,
-  PRIMARY KEY (`data_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`data_id`),
+  KEY `channel` (`channel`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
